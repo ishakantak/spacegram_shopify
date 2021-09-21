c2379e4018935d013cb937a6b7cb43105853ea25
Isha <kantakisha@gmail.com>
2021-09-21 12:29:19 -0400

    changes

---
+++ spacegram_shopify/src/App.js
@@ -0,0 +1,18 @@
+import react from 'react';
+// import ImagesDisplay from "./components/ImagesDisplay";
+// import Users from "./components/users"
+import HomePage from './components/HomePage';
+
+
+function App() {
+  return (
+    <div>
+    <HomePage />
+      </div>
+  
+
+    
+  );
+}
+
+export default App;
---
+++ spacegram_shopify/src/components/CardList.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import heartgray from './heartgray.svg';
+import heartred from './heartred.svg';
+
+const CardList = ({ title, date, url }) => {
+  const [liked, setLiked] = useState(false)
+  return (
+    <div className="cardcontainer">
+    <div className="card">
+    <div className="topContainer">
+    <div className="title">{title}</div> 
+   
+    <div className="date">{date}</div>
+    </div>
+      <img src={url} style={{width: "100%"}} />
+      {/* <div className="bottomContainer">
+      {explanation}
+      </div> */}
+      <div className="like">
+      <img
+          src={liked ? heartred : heartgray}
+          alt="like button"
+          onClick={() => setLiked((curr) => !curr)}
+        ></img>
+      </div>
+    </div> 
+
+    </div>
+  )
+}
+
+export default CardList
