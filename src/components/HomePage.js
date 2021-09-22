b4a5d9c7cea96a72f81cf19c35cfee7f5d7ebcbd
Isha <kantakisha@gmail.com>
2021-09-21 22:20:21 -0400

    changes

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,16 +5,15 @@ import CardList from './CardList';
 function HomePage() {
   const getImages = async () => {
     const res = await fetch('https://api.nasa.gov/planetary/apod?api_key=rpf2PXcZhgOIA7Tjek38tFq5HDa7a5dsUgDiuVo1&count=8');
-    const data = await res.json();
-    return data;
+    return res.json();
  }
 
  const [photos, setPhotos] = useState([]);
  const [loading, setLoading] = useState(true);
  const handleImages = () => {
    setLoading(true);
-     getImages().then((photos) => {
-         setPhotos((curr) => [...curr, ...photos]);
+     getImages().then((photoList) => {
+         setPhotos((curr) => [...curr, ...photoList]);
          setLoading(false);
      });
  };
@@ -22,44 +21,39 @@ function HomePage() {
    handleImages();
 }, []);
  
-  return (
+  return (<>
+            <div className="stars"></div>
+            <div className="stars2"></div>
+            <div className="container">
+            <h1>Spaceagram</h1>
+            <h2>Image sharing from the Frontier</h2>
+            <h3 className="headingtwo">Imported by NASA'S APOD (Astronomy Picture of the Day) </h3>
+            {/* <p style={{color:"white"}}>This is a website that display photos from NASA's APOD API.</p> */}
+            {/* <p style={{color:"white"}}>1) You can like or unlike a photo by clicking the heart icon under the photo</p>
+            <p style={{color:"white"}}>2) You can also load more photo by clicking the button at the bottom of the page</p> */}
 
-<>
-<div className="stars"></div>
-<div className="stars2"></div>
-  <div className="container">
-<h1>Spaceagram</h1>
-<h2>Image sharing from the Frontier</h2>
-<h3 className="headingtwo">Imported by NASA'S APOD (Astronomy Picture of the Day) </h3>
-{/* <p style={{color:"white"}}>This is a website that display photos from NASA's APOD API.</p> */}
-{/* <p style={{color:"white"}}>1) You can like or unlike a photo by clicking the heart icon under the photo</p>
-<p style={{color:"white"}}>2) You can also load more photo by clicking the button at the bottom of the page</p> */}
+            {/* <p>Click the like button to like the photos</p>
+            <p>Click the refresh button below to see and like more images.</p> */}
+            <div className="cardFlex">
 
-{/* <p>Click the like button to like the photos</p>
-<p>Click the refresh button below to see and like more images.</p> */}
-        <div className="cardFlex">
-           
             {photos.length > 0 &&
             photos.map(
-              (photo) =>
-                photo.media_type === "image" && (
-                  <CardList
-                    title={photo.title}
-                    date={photo.date}
-                    url={photo.url}
-                    key={photo.title}
-                    // explanation={photo.explanation}
-                  />
-                )
+            (photo) =>
+            photo.media_type === "image" && (
+            <CardList
+            title={photo.title}
+            date={photo.date}
+            url={photo.url}
+            key={photo.title}
+            // explanation={photo.explanation}
+            />
+            )
             )}
             {loading && <div className="load">Loading...</div>}
-
-</div>
-           <div className="sync"> <img src={sync} alt="load more" onClick={handleImages}></img> </div>
             </div>
-            </>
-
-  );
+            <div className="sync"> <img src={sync} alt="load more" onClick={handleImages}></img> </div>
+            </div>
+            </>);
 }
 
 export default HomePage;
